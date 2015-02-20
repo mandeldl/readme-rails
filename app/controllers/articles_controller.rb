8b4d030dd3dc5f273610065c6f287664c7b6eaea
David Mandel <MandelDL@gmail.com>
2015-02-19 22:16:26 -0500

    add update method to articles controller that changes upvote counter

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,11 @@ class ArticlesController < ApplicationController
 	def index
 		@articles = Article.order(upvotes: :desc).all
 	end
+
+	def update
+		@article = Article.find(params[:id])
+		upvotes = @article.upvotes+1
+		@article.update_attributes(upvotes:upvotes)
+		redirect_to root_path
+	end
 end
